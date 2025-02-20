65262f85faea7c7468858484cd68c2886612c5f8
CyprianSiwik <124326420+CyprianSiwik@users.noreply.github.com>
2025-02-20 16:47:52 -0500

    Initial setup

---
+++ Main.py
@@ -0,0 +1,35 @@
+#Network Design Phase 2 Programming Project
+
+#imports
+import matplotlib.pyplot
+
+#Functions
+
+    #Make_packet function
+
+    #Gives us array of packets
+def make_packet(image_path, packet_size=1024):
+    imageData = []
+    try:
+        with open(image_path, 'rb') as image_file:
+            while True:
+                packet = image_file.read(packet_size)
+                if not packet:
+                    break
+                imageData.append(packet)
+        return imageData
+    except FileNotFoundError:
+        print("File was not found.")
+
+    #Performance Plots
+
+
+
+
+
+#main
+image_path = str(input("Enter the file name:"))
+print(image_path)
+
+while True:
+    print("Select the data transfer scenario you wish to choose:")
